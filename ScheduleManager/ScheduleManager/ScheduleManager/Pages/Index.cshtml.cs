22e6cdf4e0c42a19bc5ca6f301363493bb5b2656
sdani00 <solomoneandan@gmail.com>
2023-12-27 20:39:28 +0200

    project restructuring

--- ScheduleManager/ScheduleManager/ScheduleManager/Pages/Index.cshtml.cs
+++ ScheduleManager/ScheduleManager/ScheduleManager/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ScheduleManager.Application.Pages
+namespace ScheduleManager.Pages
 {
     public class IndexModel : PageModel
     {
--- ScheduleManager/ScheduleManager/ScheduleManager/Pages/Privacy.cshtml.cs
+++ ScheduleManager/ScheduleManager/ScheduleManager/Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace ScheduleManager.Application.Pages
+namespace ScheduleManager.Pages
 {
     public class PrivacyModel : PageModel
     {
